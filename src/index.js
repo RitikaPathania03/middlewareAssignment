3f3116a2cc7540bb41b2921bab92850dbc0906d0
RitikaPathania03 <ritikapathania520@gmail.com>
2022-06-12 00:24:52 +0530

    added many files in package

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb://BookSchema:Accountancy00@cluster0.pxckc.mongodb.net:27017/?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
